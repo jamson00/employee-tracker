e529fbc3bab3d5c604f0c5042049a8788b5a6f86
jamson00 <jameson.edward.daley@gmail.com>
2023-06-27 15:24:15 -0400

    setup sql files, installed inquirer, installed mysql2

---
+++ sql/seeds.sql
@@ -0,0 +1,18 @@
+-- Insert sample departments
+INSERT INTO department (id, name) VALUES
+  (1, 'Sales'),
+  (2, 'Marketing'),
+  (3, 'Finance');
+
+-- Insert sample roles
+INSERT INTO role (id, title, salary, department_id) VALUES
+  (1, 'Sales Representative', 50000, 1),
+  (2, 'Marketing Coordinator', 45000, 2),
+  (3, 'Financial Analyst', 60000, 3);
+
+-- Insert sample employees
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
+  (1, 'John', 'Doe', 1, NULL),
+  (2, 'Jane', 'Smith', 2, 1),
+  (3, 'Mike', 'Johnson', 3, 1);
+
